162d05b90d52975235396a7f23576948de297124
Andrew Kharchyshyn <haawaplus@gmail.com>
2016-04-03 23:24:12 +0300

    Initial Commit

---
+++ AllKanjiBezierPath/AllKanjiBezierPath.h
@@ -0,0 +1,19 @@
+//
+//  AllKanjiBezierPath.h
+//  AllKanjiBezierPath
+//
+//  Created by Andriy K. on 4/3/16.
+//  Copyright © 2016 Andriy K. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AllKanjiBezierPath.
+FOUNDATION_EXPORT double AllKanjiBezierPathVersionNumber;
+
+//! Project version string for AllKanjiBezierPath.
+FOUNDATION_EXPORT const unsigned char AllKanjiBezierPathVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AllKanjiBezierPath/PublicHeader.h>
+
+
